e0dfc7e42880c775a2931c5a6741a2cda5b72954
NathanCong <congyunan@126.com>
2025-11-13 20:12:32 +0800

    feat: 初始化项目

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="main-layout">
+    <section class="main-header">
+      <GlobalHeader :title="globalHeaderTitle" :bgColor="globalHeaderBgColor" />
+    </section>
+    <section class="main-content">
+      <slot></slot>
+    </section>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import GlobalHeader from '@/components/GlobalHeader'
+
+withDefaults(
+  defineProps<{ globalHeaderTitle?: string; globalHeaderBgColor?: string }>(),
+  {
+    globalHeaderTitle: 'MainLayout',
+    globalHeaderBgColor: '#fff'
+  }
+)
+</script>
+
+<style lang="less" scoped>
+.main-layout {
+  width: 100%;
+  height: 100%;
+  // background-color: #999;
+  display: flex;
+  flex-direction: column;
+
+  .main-header {
+    width: 100%;
+    height: 60px;
+    box-sizing: border-box;
+  }
+
+  .main-content {
+    flex: 1;
+    background-color: #f2f8ff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
---
+++ src/views/home/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    欢迎使用
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="less" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+</style>
