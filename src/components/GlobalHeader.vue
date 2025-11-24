7af7c09f7e99a170fe7414212ac3ca50eca8d547
NathanCong <congyunan@126.com>
2025-11-24 13:39:12 +0800

    feat: 完成 GlobalHeader 改造

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="global-header" :style="{ backgroundColor: bgColor }">
     <section class="header-left">
-      <template v-if="showLogo">
-        <span class="header-logo"></span>
+      <template v-if="showLogo && logoSrc">
+        <img class="header-logo" :src="logoSrc" />
+      </template>
+      <template v-if="showTitle">
+        <span class="header-title">{{ title }}</span>
+      </template>
+      <template v-if="showSubTitle">
+        <span class="header-divider"></span>
+        <span class="header-subtitle">{{ subTitle }}</span>
       </template>
     </section>
-    <section class="header-right"></section>
+    <section class="header-right">
+      <template v-if="showActions">
+        <slot name="actions"></slot>
+      </template>
+    </section>
   </div>
 </template>
 
@@ -15,11 +26,21 @@ withDefaults(
     bgColor?: string
     showLogo?: boolean
     logoSrc?: string
+    showTitle?: boolean
+    title?: string
+    showSubTitle?: boolean
+    subTitle?: string
+    showActions?: boolean
   }>(),
   {
     bgColor: '#fff',
-    showLogo: false,
-    logoSrc: ''
+    showLogo: true,
+    logoSrc: '',
+    showTitle: true,
+    title: '测试标题',
+    showSubTitle: true,
+    subTitle: '测试副标题',
+    showActions: true
   }
 )
 </script>
@@ -35,15 +56,37 @@ withDefaults(
   box-sizing: border-box;
   padding: 0 20px;
 
-  .logo-wrapper {
+  .header-left {
     display: flex;
+    flex-direction: row;
     align-items: center;
-    cursor: pointer;
+    justify-content: flex-start;
+    height: 30px;
 
-    // .logo-text {
-    //   font-size: 18px;
-    //   font-weight: bold;
-    // }
+    .header-logo {
+      width: auto;
+      height: 100%;
+      margin-right: 6px;
+    }
+
+    .header-title {
+      font-weight: bold;
+      font-size: 18px;
+      line-height: 18px;
+    }
+
+    .header-divider {
+      height: 18px;
+      border-left: 1px solid #333;
+      margin: 0 10px;
+      align-self: center;
+    }
+
+    .header-subtitle {
+      font-weight: normal;
+      font-size: 16px;
+      line-height: 16px;
+    }
   }
 }
 </style>
--- src/views/test/TestPage.vue
+++ src/views/test/TestPage.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="test">
     <section class="test-header">
-      <GlobalHeader />
+      <GlobalHeader
+        :show-logo="true"
+        :logo-src="LogoSVG"
+        :show-title="true"
+        title="慧恩齐家医疗科技"
+        :show-sub-title="true"
+        sub-title="HR系统"
+        :show-actions="true"
+      >
+        <template #actions>
+          <div class="header-actions">
+            <a-button type="primary">测试</a-button>
+          </div>
+        </template>
+      </GlobalHeader>
     </section>
   </div>
 </template>
 
 <script lang="ts" setup>
 import GlobalHeader from '@/components/GlobalHeader.vue'
+import LogoSVG from './assets/images/logo.svg'
 </script>
 
 <style lang="less" scoped>
